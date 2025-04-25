04a316fa7a74dea3c43f4a17f13785c713c35cee
нассер <@NASSER>
2025-04-26 01:50:57 +0500

    Task4

---
+++ Tyuiu.NajibN.Sprint5.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.NajibN.Sprint5.Task4.V14.Lib;
+namespace Tyuiu.NajibN.Sprint5.Task4.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+        {
+            [TestMethod]
+            public void CheckedExistFile()
+            {
+                string path = @"C:\DataSprint5\InPutDataFileTask4V14.txt";
+                FileInfo fileInfo = new FileInfo(path);
+                bool fileExist = fileInfo.Exists;
+                bool wait = true;
+                Assert.AreEqual(wait, fileExist);
+            }
+        }
+    }
---
+++ Tyuiu.NajibN.Sprint5.Task4.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NajibN.Sprint5.Task4.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NajibN.Sprint5.Task4.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9adfb610-d5c0-4946-a809-0805efa2d496")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
